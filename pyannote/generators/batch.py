1a4955257c72cf9c8dda0d918ac28e8f8e5e17d4
Hervé BREDIN <bredin@limsi.fr>
2016-06-20 13:50:24 +0200

    feat: add BaseBatchGenerator base class

---
+++ pyannote/generators/batch.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# The MIT License (MIT)
+
+# Copyright (c) 2016 CNRS
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# AUTHORS
+# Hervé BREDIN - http://herve.niderb.fr
+
+
+import numpy as np
+
+
+class BaseBatchGenerator(object):
+
+    def __init__(self, fragment_generator, batch_size=32):
+        super(BaseBatchGenerator, self).__init__()
+        self.fragment_generator = fragment_generator
+        self.signature_ = self.fragment_generator.signature()
+        self.batch_size = batch_size
+
+    def preprocess(self, protocol_item):
+        raise NotImplementedError()
+
+    def process(self, fragment, signature=None):
+        raise NotImplementedError()
+
+    def __batch_new(self, signature=None):
+
+        if signature is None:
+            signature = self.signature_
+
+        if isinstance(signature, list):
+            return [self.__batch_new(signature=_signature)
+                    for _signature in signature]
+
+        elif isinstance(signature, tuple):
+            return (self.__batch_new(signature=_signature)
+                    for _signature in signature)
+
+        elif isinstance(signature, dict):
+
+            fragment_type = signature.get('type', None)
+
+            if fragment_type in set(['segment', 'boolean']):
+                return []
+
+            if fragment_type is None:
+                return {key: self.__batch_new(signature=_signature)
+                        for key, _signature in signature.items()}
+
+            raise NotImplementedError('')
+
+    def __batch_add(self, fragment, signature=None, batch=None):
+
+        if signature is None:
+            signature = self.signature_
+
+        if batch is None:
+            batch = self.batch_
+
+        if isinstance(signature, (list, tuple)):
+            for _fragment, _signature, _batch, in zip(fragment, signature, batch):
+                self.__batch_add(_fragment, signature=_signature, batch=_batch)
+
+        elif isinstance(signature, dict):
+            fragment_type = signature.get('type', None)
+
+            if fragment_type == 'segment':
+                processed = self.process(fragment, signature=signature)
+                batch.append(processed)
+
+            elif fragment_type == 'boolean':
+                processed = self.process(fragment, signature=signature)
+                batch.append(processed)
+
+            else:
+                for key in signature:
+                    self.__batch_add(fragment[key],
+                                     signature=signature[key],
+                                     batch=batch[key])
+
+    def __batch_pack(self, signature=None, batch=None):
+
+        if signature is None:
+            signature = self.signature_
+
+        if batch is None:
+            batch = self.batch_
+
+        if isinstance(signature, list):
+            return [self.__batch_pack(signature=_signature, batch=_batch)
+                    for _signature, _batch in zip(signature, batch)]
+
+        elif isinstance(signature, tuple):
+            return (self.__batch_pack(signature=_signature, batch=_batch)
+                    for _signature, _batch in zip(signature, batch))
+
+        elif isinstance(signature, dict):
+
+            fragment_type = signature.get('type', None)
+
+            if fragment_type in set(['segment', 'boolean']):
+                return np.stack(batch)
+
+            else:
+                return {key: self.__batch_pack(signature=signature[key],
+                                               batch=batch[key])
+                        for key in signature.items()}
+
+
+    def __call__(self, protocol):
+
+        batch_size = 0
+        self.batch_ = self.__batch_new()
+
+        while True:
+            for protocol_item in protocol.train_iter():
+
+                self.preprocess(protocol_item)
+
+                for fragment in self.fragment_generator(protocol_item):
+
+                    self.__batch_add(fragment)
+                    batch_size += 1
+
+                    if batch_size == self.batch_size:
+                        yield self.__batch_pack()
+                        self.batch_ = self.__batch_new()
+                        batch_size = 0
